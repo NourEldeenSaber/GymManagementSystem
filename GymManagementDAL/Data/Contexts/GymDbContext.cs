13f761b5094b288e1cc0c791606ed3ee907e4584
NourSaber <nour01saber@gmail.com>
2026-01-12 22:00:21 +0200

    Add DbContext

---
+++ GymManagementDAL/Data/Contexts/GymDbContext.cs
@@ -0,0 +1,33 @@
+﻿using GymManagementDAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace GymManagementDAL.Data.Contexts
+{
+    internal class GymDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = MSI\\SQLEXPRESS; DataBase = GymManagement; Trusted_Connection = True; TrustServerCertificate = True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); 
+        }
+
+        #region DbSets
+
+        public DbSet<Member> Members { get; set; }
+        public DbSet<HealthRecord> HealthRecords { get; set; }
+        public DbSet<Trainer> Trainers { get; set; }
+        public DbSet<Plan> Plans { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<MemberShip> MemberShips { get; set; }
+        public DbSet<MemberSession> MemberSessions { get; set; }
+
+        #endregion
+    
+    }
+}
