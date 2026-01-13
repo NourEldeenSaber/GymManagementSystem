aa167875c3c8410c36a2e6ffc0c2700452ab05e0
NourSaber <nour01saber@gmail.com>
2026-01-13 11:57:27 +0200

    Member Repository [Interface and Class]

---
+++ GymManagementDAL/Repositories/Interfaces/IMemberRepository.cs
@@ -0,0 +1,22 @@
+﻿using GymManagementDAL.Entities;
+
+namespace GymManagementDAL.Repositories.Interfaces
+{
+    internal interface IMemberRepository
+    {
+        // GetAll
+        IEnumerable<Member> GetAll();
+
+        // GetById
+        Member? GetById(int id);
+
+        // Add
+        int Add(Member member);
+
+        // Update
+        int Update(Member member);
+
+        // Delete
+        int Delete(int id);
+    }
+}
