375b79c231fd41eafd6271b04d8edae9a633a273
NourSaber <nour01saber@gmail.com>
2026-02-22 17:48:11 +0200

    MemberController-Create[Get-Post]

--- GymManagementPL/Controllers/MemberController.cs
+++ GymManagementPL/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
 ﻿using GymManagementBLL.Services.Interfaces;
+using GymManagementBLL.ViewModels.MemberViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GymManagementPL.Controllers
@@ -27,31 +28,76 @@ namespace GymManagementPL.Controllers
 
         public IActionResult MemberDetails(int id) 
         { 
-            if(id <= 0) 
+            if(id <= 0)
+            {
+                TempData["ErrorMessage"] = "Id of Member cannot be 0 or Negative number";
                 return RedirectToAction(nameof(Index));
+            }
         
             var member = _memberService.GetMemberDetails(id);
 
             if(member == null)
+            {
+                TempData["ErrorMessage"] = "Member Not Found";
                 return RedirectToAction(nameof(Index));
+            }
 
             return View(member);
         }
 
         public IActionResult HealthRecordDetails(int id)
         {
-            if(id <= 0 )
+            if(id <= 0)
+            {
+                TempData["ErrorMessage"] = "Id of Member cannot be 0 or Negative number";
                 return RedirectToAction(nameof(Index));
+            }
 
             var HealthRecord = _memberService.GetMemberHealthRecordDetails(id);
             
             if(HealthRecord is  null)
+            {
+                TempData["ErrorMessage"] = "HealthRecord Not Found";
                 return RedirectToAction(nameof(Index));
-
+            }
             return View(HealthRecord);
 
         }
-        
+
         #endregion
+
+        #region Create Member
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateMember(CreateMemberViewModel CreatedMember)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("DataInvalid", "Check Data And Missing Field");
+                return View(nameof(Create),CreatedMember);
+            }
+
+            bool Result = _memberService.CreateMember(CreatedMember);
+            if (Result)
+            {
+                TempData["SuccessMessage"] = "Member Created Successfully";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Member Failed to Create, check phone and email ";
+
+            }
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        #endregion
+
+
     }
 }
