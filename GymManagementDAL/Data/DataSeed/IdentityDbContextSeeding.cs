3baac05e5d2f92a2422d9bd75cd5d55f4de38dee
NourSaber <nour01saber@gmail.com>
2026-03-30 11:24:39 +0200

    IdentityMainAdminSeeding

---
+++ GymManagementDAL/Data/DataSeed/IdentityDbContextSeeding.cs
@@ -0,0 +1,67 @@
+﻿
+
+using GymManagementDAL.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace GymManagementDAL.Data.DataSeed
+{
+    public static class IdentityDbContextSeeding
+    {
+        public static bool seedData(RoleManager<IdentityRole> roleManager , UserManager<ApplicationUser> userManager)
+        {
+            try
+            {   
+                var HasUsers = userManager.Users.Any();
+                var HasRoles = roleManager.Roles.Any();
+                if (HasUsers && HasRoles) return false;
+
+                if (!HasRoles)
+                {
+                    var Roles = new List<IdentityRole>()
+                    {
+                        new(){Name = "SuperAdmin"},
+                        new(){Name = "Admin"},
+                    };
+
+                    foreach (var Role in Roles)
+                    { 
+                        if(!roleManager.RoleExistsAsync(Role.Name!).Result)
+                            roleManager.CreateAsync(Role).Wait();
+                    }
+                }
+
+                if (!HasUsers)
+                {
+                    var MainAdmin = new ApplicationUser()
+                    {
+                        FirstName = "Nour",
+                        LastName = "Saber",
+                        UserName = "NourSaber",
+                        Email = "NourSaber@gmail.com",
+                        PhoneNumber = "01010101010"
+                    };
+                    userManager.CreateAsync(MainAdmin,"P@ssw0rd").Wait();
+                    userManager.AddToRoleAsync(MainAdmin, "SuperAdmin").Wait();
+
+                    var Admin = new ApplicationUser()
+                    {
+                        FirstName = "Yasser",
+                        LastName = "Mohamed",
+                        UserName = "YasserMohamed",
+                        Email = "YasserMohamed@gmail.com",
+                        PhoneNumber = "01010101010"
+                    };
+                    userManager.CreateAsync(Admin, "P@ssw0rd").Wait();
+                    userManager.AddToRoleAsync(Admin, "Admin").Wait();
+
+                }
+               
+                return true;
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"Seeding Failed : {ex}");
+                return false;
+            }
+        }
+    }
+}
